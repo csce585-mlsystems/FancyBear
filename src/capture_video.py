17efe32cda5163a4389b8cde361a0b5e7523f6ed
Travisandre <70145967+Travisandre@users.noreply.github.com>
2024-12-11 19:25:00 -0500

    Delete src directory

--- src/capture_video.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-#Video capture
-def capture_video(video_source = 0):
-    capture = cv2.VideoCapture(video_source)
-    if not capture.isOpened():
-        print("Error: Cannot Open Video Source.")
-        return None
-    return capture
